e29148936f1cbab2d7b25288db5eb8439128a909
ronanc02 <chesnelr@gmail.com>
2021-11-14 20:42:25 +0900

    Use lists in Kotlin

--- app/src/main/java/com/example/winetasting/MainActivity.kt
+++ app/src/main/java/com/example/winetasting/MainActivity.kt
@@ -13,18 +13,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val whiteWineBottle = White("Pouilly Fusse", "Chardonnay")
+        val wineList1 = wineList(1)
+        wineList1.addBottle(whiteWineBottle)
+
         binding.buttonCheck.setOnClickListener{
             val selectedId = binding.radioGroup1.checkedRadioButtonId
             binding.amountText.text = when (selectedId) {
                 R.id.activity_main_q1_rb1 -> "Red"
-                R.id.activity_main_q1_rb2 -> "White"
+                R.id.activity_main_q1_rb2 -> whiteWineBottle.toString()
                 else -> "Rose"
             }
         }
     }
 
     private fun amountBottle() {
-        val bottle = Bottle(6)
+        val bottle = BottleGen(6)
         val bottleAmount = bottle.amount()
 
         binding.amountText.text = bottleAmount.toString()
@@ -32,8 +36,33 @@ class MainActivity : AppCompatActivity() {
 }
 
 
-class Bottle (val amountRanges: Int){
+class BottleGen (val amountRanges: Int){
     fun amount(): Int {
         return (1..amountRanges).random()
     }
+}
+open class Bottle(val name: String, val price: Int)
+
+class Red : Bottle("Red wine", 10)
+
+class White(vararg val types: String) : Bottle("White wine", 7) {
+    override fun toString(): String {
+        if (types.isEmpty()) {
+            return "No $name"
+        } else {
+            return types.joinToString()
+        }
+    }
+}
+
+class Rose : Bottle("Rose wine", 4)
+
+class wineList(val listNumber: Int) {
+    private val bottleList = mutableListOf<Bottle>()
+
+    fun addBottle(newBottle: Bottle) : wineList {
+        bottleList.add(newBottle)
+        return this
+    }
+
 }
