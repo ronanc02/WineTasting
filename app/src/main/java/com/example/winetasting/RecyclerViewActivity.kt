42fb8e9117914169a108e3c76c26e9084024c2b7
ronanc02 <chesnelr@gmail.com>
2021-11-15 06:03:46 +0900

    Use RecyclerView to display a scrollable list

--- app/src/main/java/com/example/winetasting/RecyclerViewActivity.kt
+++ app/src/main/java/com/example/winetasting/RecyclerViewActivity.kt
@@ -2,6 +2,9 @@ package com.example.winetasting
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.winetasting.adapter.ItemAdapter
+import com.example.winetasting.data.Datasource
 import com.example.winetasting.databinding.ActivityRecyclerviewBinding
 
 class RecyclerViewActivity : AppCompatActivity() {
@@ -10,6 +13,11 @@ class RecyclerViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityRecyclerviewBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val myDataset = Datasource().loadWines()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
         }
 
 }
